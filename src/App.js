65e32e74087ca69caa6240b8ea0277273dce3005
Egor Tanachev <egor1284@gmail.com>
2024-04-10 12:48:11 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   return (
     <div className="App">
         <Router>
+          <div className='header__height'></div>
           <Header />
 
           <Routes>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import './Header.css'
 import { Link } from 'react-router-dom';
 
+import logo from './img/logo.svg';
+import icon1 from './img/icon1.svg';
+import icon2 from './img/icon2.svg';
+import icon3 from './img/icon3.svg';
+import burger from './img/burger.svg';
+
 const Header = () => {
     return (
         <header className="header" id="header">
@@ -22,7 +28,33 @@ const Header = () => {
             </div>
             <div className="header__bottom">
                 <div className="container">
-                    
+                    <Link to="/">
+                        <img src={logo} alt="logo" />
+                    </Link>
+                    <div className="header__products">
+                        <Link className="header__catalog" to="/catalog">
+                            <img src={burger} alt="burger" />
+                            <span className="font-size-16 font-weight-500">Каталог</span>
+                        </Link>
+                        <form className="header__find">
+                            <input type="text" placeholder="Поиск" autofocus/>
+                            <input type="submit" value="Найти" title="Начать поиск"/>
+                        </form>
+                    </div>
+                    <div className="header__link">
+                        <Link to="/account">
+                            <img src={icon1} alt="account" />
+                            <span className="font-size-12">Профиль</span>
+                        </Link>
+                        <Link to="/basket">
+                            <img src={icon2} alt="basket" />
+                            <span className="font-size-12">Корзина</span>
+                        </Link>
+                        <Link to="/info">
+                            <img src={icon3} alt="info" />
+                            <span className="font-size-12">Данные</span>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </header>
